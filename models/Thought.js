829684463b740f17fa3a69aaf49f70fefed8b46b
Samantha Wolff <samanthawolff07@gmail.com>
2022-06-03 16:01:22 -0500

    set up Reaction model

--- models/Thought.js
+++ models/Thought.js
@@ -1,4 +1,31 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
+
+
+const ReactionSchema = new Schema ({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: ()=> new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+},
+{
+    toJSON: {
+        getters: true
+    }
+});
 
 
 const ThoughtSchema = new Schema ({
@@ -16,7 +43,7 @@ const ThoughtSchema = new Schema ({
         type: String,
         required: true
     },
-    reactions: []
+    reactions: [ ReactionSchema ]
 },
 {
     toJSON: {
@@ -24,6 +51,11 @@ const ThoughtSchema = new Schema ({
         getters: true
     },
     id: false
+});
+
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
 });
 
 
