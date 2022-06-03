5e0c50090034c95d5624eb8f700ec47d91a00a6d
Samantha Wolff <samanthawolff07@gmail.com>
2022-06-03 15:52:11 -0500

    set up the Thought model

---
+++ models/Thought.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require('mongoose');
+
+
+const ThoughtSchema = new Schema ({
+    thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: []
+},
+{
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+});
+
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
