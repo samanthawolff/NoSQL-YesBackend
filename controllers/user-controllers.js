e6c8f5561a1288a1753e9b98d444a5ede8f19d0f
Samantha Wolff <samanthawolff07@gmail.com>
2022-06-05 09:42:26 -0500

    working update user route

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -36,6 +36,22 @@ const userController = {
             console.log(err);
             res.status(500).json(err);
         });
+    },
+
+    // Update a user
+    updateUser({ params, body}, res) {
+        User.findOneAndUpdate({ _id: params.id}, body, { new: true })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id!' })
+                return;
+            }
+            res.json(dbUserData)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err)
+        });
     }
 }
 
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -5,7 +5,7 @@ const {
     getAllUsers,
     getUserById,
     createUser,
-    //updateUser,
+    updateUser,
     //deleteUser,
     //addFriend,
     //deleteFriend
@@ -17,7 +17,7 @@ router.route('/').get(getAllUsers).post(createUser);
 
 
 // api/users/id
-router.route('/:id').get(getUserById)//.put(updateUser).delete(deleteUser);
+router.route('/:id').get(getUserById).put(updateUser)//.delete(deleteUser);
 
 
 // api/users/userId/friends/friendId
